b5f110fe5f52703b2120d318c1f24422b07a3e5c
lanibon <nguyenlan13@gmail.com>
2020-06-28 01:17:16 -0700

    more blog links

--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -5,7 +5,12 @@ function Blog() {
     <div>
       <h1>Blogs</h1>
         <h2><a href="https://nguyenlan13.github.io/selection_sort">Selection Sort</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/bubble_sort">Bubble Sort</a></h2>
         <h2><a href="https://nguyenlan13.github.io/stacks_and_queues">Stacks and Queues</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/linked_lists">Linked Lists</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/hash_tables">Hash Tables</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/big-o_simplified">Big-O Simplified</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/react_hooks">React Hooks</a></h2>
     </div>
   );
 }
