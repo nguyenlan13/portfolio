ecb3d3b36824aad4ec8aaf3ebf27d23087cbb520
lanibon <nguyenlan13@gmail.com>
2020-06-28 01:06:37 -0700

    add blog links

--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -4,6 +4,8 @@ function Blog() {
   return (
     <div>
       <h1>Blogs</h1>
+        <h2><a href="https://nguyenlan13.github.io/selection_sort">Selection Sort</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/stacks_and_queues">Stacks and Queues</a></h2>
     </div>
   );
 }
