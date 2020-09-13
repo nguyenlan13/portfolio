3fee0bacc2fba53e35f299a02545fa420660ae78
lanibon <nguyenlan13@gmail.com>
2020-09-12 17:47:17 -0700

    change css for footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Footer() {
     return(
-        <div>
+        <div className="footer">
         <div>Lan Nguyen (c)</div>
         <div>2020</div>
         </div>
