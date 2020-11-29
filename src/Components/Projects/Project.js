a2567af5cb7c20efaa9d9c78f441384800fe4f4b
lanibon <nguyenlan13@gmail.com>
2020-11-28 20:50:36 -0800

    add new project link

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -5,7 +5,7 @@ function Project() {
   return (
     <div>
      <ul>
-     <h2><a href="https://github.com/nguyenlan13/toolSet">toolSet</a></h2>
+     <h2><a href="https://toolset-frontend.herokuapp.com/">toolSet</a> | <a href="https://github.com/nguyenlan13/toolSet">Code</a></h2>
      <h2><a href="https://github.com/nguyenlan13/imPACT">imPACT</a></h2>
      <h2><a href="https://github.com/nguyenlan13/shelf_development">Shelf Development</a></h2>
 
