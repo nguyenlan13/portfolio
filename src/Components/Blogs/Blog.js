3f85f852a466734153739acbb186f5431ff82d77
lanibon <nguyenlan13@gmail.com>
2020-12-02 05:15:30 -0800

    add new blog links

--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -4,6 +4,13 @@ function Blog() {
   return (
     <div>
       <h1>Small Bytes</h1>
+        <h2><a href="https://nguyenlan13.github.io/react_components">React Components</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/redux_thunk">Redux Thunk</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/active_record_associations">Active Record Associations</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/model_view_controller">Model-View-Controller</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/bits_and_bytes">Bits and Bytes</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/merge_sort">Merge Sort</a></h2>
+        <h2><a href="https://nguyenlan13.github.io/what_is_redux">What is Redux?</a></h2>
         <h2><a href="https://nguyenlan13.github.io/quick_sort">Quick Sort</a></h2>
         <h2><a href="https://nguyenlan13.github.io/selection_sort">Selection Sort</a></h2>
         <h2><a href="https://nguyenlan13.github.io/bubble_sort">Bubble Sort</a></h2>
@@ -12,6 +19,7 @@ function Blog() {
         <h2><a href="https://nguyenlan13.github.io/hash_tables">Hash Tables</a></h2>
         <h2><a href="https://nguyenlan13.github.io/big-o_simplified">Big-O Simplified</a></h2>
         <h2><a href="https://nguyenlan13.github.io/react_hooks">React Hooks</a></h2>
+
     </div>
   );
 }
